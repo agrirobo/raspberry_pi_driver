7c3e304220114c5656faab184ec149b92740f73b
YamaCIR-KIT <httperror@404-notfound.jp>
2016-12-09 00:05:14 +0900

    Edit something

--- src/raspberry_pi_driver.cpp
+++ src/raspberry_pi_driver.cpp
@@ -16,8 +16,11 @@ class RPiDriver {
   double tolerance_;
   static constexpr double pi_ {3.1415};
 
-  int start_stop_, interrupt_, reduction_ratio_, wheel_radius_, pulse_per_spin_;
-  static int cw_ccw_, pulse_;
+  int start_stop_, interrupt_;
+  double reduction_ratio_, wheel_radius_, pulse_per_spin_;
+  static int cw_ccw_;
+  static long pulse_;
+  static bool is_cw_;
 
 public:
   RPiDriver(ros::NodeHandle& node_handle)
@@ -26,17 +29,25 @@ public:
       command_ {},
       odometry_ {}
   {
-    if (wiringPiSetupPhys() < 0) ROS_ERROR_STREAM("[ERROR] wiringPiSetupPhys()");
+    if (wiringPiSetupPhys() < 0) ROS_ERROR_STREAM("wiringPiSetupPhys()");
 
-    node_handle.getParam("tolerance", tolerance_);
+    ros::NodeHandle pnh {"~"};
 
-    node_handle.getParam("cw_ccw", cw_ccw_);
-    node_handle.getParam("start_stop", start_stop_);
-    node_handle.getParam("interrupt", interrupt_);
+    assert(pnh.getParam("tolerance", tolerance_));
+    assert(pnh.getParam("cw_ccw", cw_ccw_));
+    assert(pnh.getParam("start_stop", start_stop_));
+    assert(pnh.getParam("interrupt", interrupt_));
+    assert(pnh.getParam("reduction_ratio", reduction_ratio_));
+    assert(pnh.getParam("wheel_radius", wheel_radius_));
+    assert(pnh.getParam("pulse_per_spin", pulse_per_spin_));
 
-    node_handle.getParam("reduction_ratio", reduction_ratio_);
-    node_handle.getParam("wheel_radius", wheel_radius_);
-    node_handle.getParam("pulse_per_spin", pulse_per_spin_);
+    ROS_INFO_STREAM("tolerance: " << tolerance_);
+    ROS_INFO_STREAM("cw_ccw: " << cw_ccw_);
+    ROS_INFO_STREAM("start_stop: " << start_stop_);
+    ROS_INFO_STREAM("intertupt:" << reduction_ratio_);
+    ROS_INFO_STREAM("wheel_radius:" << wheel_radius_);
+    ROS_INFO_STREAM("pulse_per_spin: " << pulse_per_spin_);
+    ROS_INFO_STREAM("reduction_ratio: " << reduction_ratio_);
 
     pinMode(cw_ccw_, OUTPUT);
     pinMode(start_stop_, OUTPUT);
@@ -45,30 +56,42 @@ public:
 
   void write()
   {
-    if ((cw_ccw_ = command_.x - odometry_.pose.pose.position.x) >= 0) digitalWrite(cw_ccw_, HIGH);
+    double distance {command_.x - odometry_.pose.pose.position.x};
+
+    if (is_cw_ = (distance > 0)) digitalWrite(cw_ccw_, HIGH);
     else digitalWrite(cw_ccw_, LOW);
 
-    if (abs(cw_ccw_) > tolerance_) digitalWrite(start_stop_, HIGH);
+    if (RPiDriver::abs(distance) > tolerance_) digitalWrite(start_stop_, HIGH);
     else digitalWrite(start_stop_, LOW);
+
+    odometry_.pose.pose.position.x = pulse_ * wheel_radius_ / pulse_per_spin_ / reduction_ratio_;
+    pub_.publish(odometry_);
   }
 
 private:
   void callback(const geometry_msgs::PointConstPtr& command)
   {
     command_ = *command;
-    odometry_.pose.pose.position.x = pulse_ * wheel_radius_ * pulse_per_spin_ * reduction_ratio_;
-    pub_.publish(odometry_);
+    ROS_INFO_STREAM("command: " << command_);
   }
 
   static void interrupt()
   {
-    if (cw_ccw_ >= 0) pulse_++;
+    if (is_cw_) pulse_++;
     else pulse_--;
+    // ROS_INFO_STREAM(pulse_);
+  }
+
+  template <typename T>
+  static T abs(const T& value)
+  {
+    return value < 0 ? -value : value;
   }
 };
 
 int RPiDriver::cw_ccw_ {0};
-int RPiDriver::pulse_ {0};
+long RPiDriver::pulse_ {0};
+bool RPiDriver::is_cw_ {0};
 
 int main(int argc, char** argv)
 {
@@ -81,6 +104,7 @@ int main(int argc, char** argv)
   while (ros::ok()) {
     driver.write();
     rate.sleep();
+    ros::spinOnce();
   }
 
   return 0;
