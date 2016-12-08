7246728960d47adb4df11e009fedfeecff614ff7
YamaCIR-KIT <httperror@404-notfound.jp>
2016-12-08 16:44:51 +0900

    Change some members to static

--- src/raspberry_pi_driver.cpp
+++ src/raspberry_pi_driver.cpp
@@ -16,20 +16,16 @@ class RPiDriver {
   double tolerance_;
   static constexpr double default_tolerance_ {0.1};
 
-  size_t cw_ccw_, start_stop_, interrupt_;
-  static constexpr size_t default_cw_ccw_ {1}; // TODO
-  static constexpr size_t default_start_stop_ {2}; // TODO
-  static constexpr size_t default_interrupt_ {3}; // TODO
-
-  long pulse_;
+  int start_stop_, interrupt_;
+  static int cw_ccw_;
+  static int pulse_;
 
 public:
   RPiDriver(ros::NodeHandle& node_handle)
     : sub_ {node_handle.subscribe<geometry_msgs::Point>("sub_topic", 1, &RPiDriver::callback, this)},
       pub_ {node_handle.advertise<nav_msgs::Odometry>("pub_topic", 1)},
       command_ {},
-      odometry_ {},
-      pulse_ {0}
+      odometry_ {}
   {
     // wirinPiSetuphogehoge // TODO
 
@@ -53,12 +49,15 @@ public:
 private:
   void callback(const geometry_msgs::PointConstPtr& command) { command_ = *command; }
 
-  void interrupt()
+  static void interrupt()
   {
     if (cw_ccw_ >= 0) pulse_++;
     else pulse_--;
   }
 };
+
+int RPiDriver::cw_ccw_ {0};
+int RPiDriver::pulse_ {0};
 
 int main(int argc, char** argv)
 {
